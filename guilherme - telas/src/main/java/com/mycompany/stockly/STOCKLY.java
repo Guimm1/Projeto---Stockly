a0175fa28004628b269b55a2c7454449649cadba
Carlos Eduardo <carlos.e.marques10@aluno.senai.br>
2025-06-16 20:10:57 -0300

    telas

---
+++ guilherme - telas/src/main/java/com/mycompany/stockly/STOCKLY.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stockly;
+
+/**
+ *
+ * @author g.magri
+ */
+public class STOCKLY {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
