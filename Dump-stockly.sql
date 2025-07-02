6d665f5d651cb25c662af09f4dc38ef643dfe60e
Carlos Eduardo <carlos.e.marques10@aluno.senai.br>
2025-07-01 21:11:49 -0300

    Dump mysql

--- Dump-stockly.sql
+++ Dump-stockly.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `stocklys` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `stocklys`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: stocklys
@@ -192,4 +194,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-13 20:10:38
+-- Dump completed on 2025-06-30 21:44:25
